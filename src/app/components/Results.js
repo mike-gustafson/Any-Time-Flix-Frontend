2f5ecd1b94e47ee555b5011c679a283acb9b8963
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-12-20 21:49:19 -0800

    FEAT: improve layout and styling of results component

--- src/app/components/Results.js
+++ src/app/components/Results.js
@@ -7,7 +7,7 @@ export default function Results() {
     const [selectedMovieId, setSelectedMovieId] = useState(null);
 
     useEffect(() => {
-        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/movies/now-playing`)
+        fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/movies/popular`)
             .then((res) => res.json())
             .then((data) => setData(data));
     }, []);
@@ -23,24 +23,32 @@ export default function Results() {
     if (!data) return <p>Loading or no data available...</p>;
     return (
         <div className={style.container}>
-            {data.results.map((movie) => (
-                <div key={movie.id} className={selectedMovieId === movie.id ? style.expandedBox : style.box} onClick={() => handleMovieClick(movie.id)}>
-                    <div className={style.body}>
-                        <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={200} height={300} className={style.image}/>
+            {data.results.map((movie) => {
+                const rating = movie.vote_average.toFixed(1);
+                const releaseDate = new Date(movie.release_date);
+                const formattedReleaseDate = `${(releaseDate.getMonth() + 1).toString().padStart(2, '0')}/${releaseDate.getDate().toString().padStart(2, '0')}/${releaseDate.getFullYear()}`;
+                return (
+                    <div key={movie.id} className={selectedMovieId === movie.id ? style.expandedBox : style.box} onClick={() => handleMovieClick(movie.id)}>
+                        <div className={style.imageContainer}>
+                            <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} width={175} height={262.5} className={style.image} alt="" />
+                        </div>
                         {selectedMovieId === movie.id && (
                             <div className={style.details}>
-                                <p className={style.summary}>{movie.overview}
-                                <p className={style.title}>{movie.title}</p>
-                                <div className={style.info}>
-                                    <p className={style.rating}>Average Rating: {movie.vote_average}</p>
-                                    <p className={style.releaseDate}>Release Date: {movie.release_date}</p>
+                                <div className={style.titleContainer}>
+                                    <span className={style.title} title={movie.title}>{movie.title}</span>
                                 </div>
-                                    </p>
+                                <div className={style.summaryContainer}>
+                                    <span className={style.summary}>{movie.overview}</span>
+                                </div>
+                                <div className={style.infoContainer}>
+                                    <span className={style.stat}>Rating: {rating}</span>
+                                    <span className={style.stat}>Released: {formattedReleaseDate}</span>
+                                </div>
                             </div>
                         )}
                     </div>
-                </div>
-            ))}
+                );
+            })}
         </div>
     );
 }
