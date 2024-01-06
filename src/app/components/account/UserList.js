38614d2f8907229c1ab89d8ad6e28e9f58962f32
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2024-01-05 22:27:59 -0800

    added remove from list functionality.

--- src/app/components/account/UserList.js
+++ src/app/components/account/UserList.js
@@ -1,9 +1,25 @@
 import Image from 'next/image';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
+export default function UserList({ list, listName, dataProp, onUpdateList }) {
+  
+  const handleRemoveFromListClick = async (event, movieId) => {
+    event.preventDefault();
+    try {
+      const response = await axios.put(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/removeFromList/${listName}/${dataProp._id}`, { movieId });
+      if (response.status === 200) {
+        // Call the callback function to update the list in the parent component
+        onUpdateList(movieId);
+      } else {
+        console.log('Error removing movie, code:', response.status);
+      }
+    } catch (error) {
+      console.error('Error removing movie from list', error);
+    }
+  }
 
-export default function UserList({list}) {
-    return (
-    
+  return (
     <div>
       {list.map(movie => (
         <div key={movie._id} style={{ display: 'flex', marginBottom: '20px', border: '1px solid #ccc', padding: '10px', width: '50%'}}>
@@ -18,9 +34,10 @@ export default function UserList({list}) {
           <div>
             <h3>{movie.original_title}</h3>
             <p>{movie.overview}</p>
+            <h6 onClick={(event) => handleRemoveFromListClick(event, movie.id)}>Remove from {listName}</h6>
           </div>
         </div>
       ))}
     </div>
-    )
+  )
 }
